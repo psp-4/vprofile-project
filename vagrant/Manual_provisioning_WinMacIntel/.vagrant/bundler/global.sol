1a9d86299eb26b8bfc568c2b821115ea146c1221
psp4 <cod.psp4@gmail.com>
2023-10-04 10:16:40 +0530

    My Commit

---
+++ vagrant/Manual_provisioning_WinMacIntel/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["vagrant-hostmanager",["= 1.8.10"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.31.0"]]],"checksum":"d47235bb038635da791e33c528f6bae44700888cd22905f56572086c5fd4fb3c","vagrant_version":"2.3.7"}
